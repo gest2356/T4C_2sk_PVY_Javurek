c9bd55a9ac4f8a7adb762ab59fe84628e9549d51
Vojtěch Javůrek <vojta.javurek@gmail.com>
2026-01-05 09:40:24 +0100

    javascript added pls send help

---
+++ 9_JavaScript/scripts.js
@@ -0,0 +1,24 @@
+let lol = [10, 20, 30, 40]
+lol.forEach(element => {
+    console.log(element)
+});
+
+for (let i = 0; i < lol.length; i++)
+{
+    if (lol[i] == 30)
+    {
+        console.log("nic")                
+    }
+    else
+        console.log(lol[i])
+}
+
+let use = {
+    name: "Pepa",
+    id: 1,
+};
+
+JSON.stringify(use)
+
+console.log(use)
+
