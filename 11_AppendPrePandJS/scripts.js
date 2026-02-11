0718f1de430aa5673b9b76fb1b2828215a28bc50
gest2356 <vojta.javurek@gmail.com>
2026-02-11 17:35:03 +0100

    du DOne Am good

--- 11_AppendPrePandJS/scripts.js
+++ 11_AppendPrePandJS/scripts.js
@@ -1,19 +1,37 @@
 const upArrowsEls = document.querySelectorAll('.upArrow');
 const downArrowsEls = document.querySelectorAll('.downArrow');
+const liContainerEls = document.getElementById('List')
 
-
-function toTheThing(){
+function doTheThing(){
     upArrowsEls.forEach(el => {
         el.addEventListener('click', e => {
-            let listEl = e.target.previousSibling;
-            console.log(listEl);
+            let listEl = e.target.parentElement;
+
+            let listAboveEl = listEl.previousElementSibling
+
+            liContainerEls.insertBefore(listEl, listAboveEl)
         })
     })
     downArrowsEls.forEach(el => {
         el.addEventListener('click', e => {
 
+            let listEl = e.target.parentElement;
+
+            let listBelowEl = listEl.nextElementSibling
+
+
+            try {
+                listBelowEl.after(listEl)
+            } catch (error) {
+                let firstChild = liContainerEls.firstChild
+
+                liContainerEls.insertBefore(listEl, firstChild)
+            } finally {
+                console.log("provedeno")
+            }
+
         })
     })
 }
 
-console.log(upArrowsEls);
+doTheThing();
