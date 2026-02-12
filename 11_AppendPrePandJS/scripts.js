49f06111cbda58c2d5ebc5ddbeb47f60d5d10c26
Vojtěch Javůrek <javurekvojtech@sssvt.cz>
2026-02-12 08:32:13 +0100

    drawinf thing proggers

--- 11_AppendPrePandJS/scripts.js
+++ 11_AppendPrePandJS/scripts.js
@@ -1,37 +1,79 @@
 const upArrowsEls = document.querySelectorAll('.upArrow');
 const downArrowsEls = document.querySelectorAll('.downArrow');
 const liContainerEls = document.getElementById('List')
+const imutListEl = document.getElementById('input')
+const buttonEl = document.getElementById('button')
 
 function doTheThing(){
-    upArrowsEls.forEach(el => {
-        el.addEventListener('click', e => {
-            let listEl = e.target.parentElement;
-
-            let listAboveEl = listEl.previousElementSibling
-
-            liContainerEls.insertBefore(listEl, listAboveEl)
+    const upArrowsEls = document.querySelectorAll('.upArrow');
+    const downArrowsEls = document.querySelectorAll('.downArrow');
+    const liContainerEls = document.getElementById('List')
+    
+        upArrowsEls.forEach(el => {
+            el.addEventListener('click', e => {
+                let listEl = e.target.parentElement;
+    
+                let listAboveEl = listEl.previousElementSibling
+    
+                liContainerEls.insertBefore(listEl, listAboveEl)
+                
+                console.log("upClick")
+            })
         })
-    })
-    downArrowsEls.forEach(el => {
-        el.addEventListener('click', e => {
-
-            let listEl = e.target.parentElement;
-
-            let listBelowEl = listEl.nextElementSibling
-
-
-            try {
-                listBelowEl.after(listEl)
-            } catch (error) {
-                let firstChild = liContainerEls.firstChild
-
-                liContainerEls.insertBefore(listEl, firstChild)
-            } finally {
-                console.log("provedeno")
-            }
-
+        downArrowsEls.forEach(el => {
+            el.addEventListener('click', e => {
+    
+                let listEl = e.target.parentElement;
+    
+                let listBelowEl = listEl.nextElementSibling
+    
+    
+                try {
+                    listBelowEl.after(listEl)
+                } catch (error) {
+                    let firstChild = liContainerEls.firstChild
+    
+                    liContainerEls.insertBefore(listEl, firstChild)
+                } finally {
+                    console.log("provedeno")
+                }
+                console.log("downClick")
+    
+            })
         })
-    })
 }
 
+function addNewThingToList(){
+    buttonEl.addEventListener('click', e => {
+        let newListItem = document.createElement('li')
+
+        newListItem.innerHTML = imutListEl.value
+        newListItem.classList.add('ListItem')
+
+        let bottomArrow = document.createElement('button')
+        bottomArrow.innerHTML = "↓"
+        bottomArrow.classList.add('downArrow')
+
+        let upArrow = document.createElement('button')
+        upArrow.innerHTML = "↑"
+        upArrow.classList.add('upArrow')
+
+        newListItem.appendChild(bottomArrow)
+        newListItem.appendChild(upArrow)
+
+        liContainerEls.appendChild(newListItem)
+        
+        doTheThing()
+    })
+
+}
+
+buttonEl.addEventListener('click', e => {
+
+    
+    doTheThing()
+
+});
+
 doTheThing();
+addNewThingToList();
