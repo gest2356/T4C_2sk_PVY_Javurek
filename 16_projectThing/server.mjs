3a9aedb676ae306870f1bf4badebfe79f6618d8a
Vojtěch Javůrek <javurekvojtech@sssvt.cz>
2026-03-12 09:40:11 +0100

    Added: mysql DB connection

--- 16_projectThing/server.mjs
+++ 16_projectThing/server.mjs
@@ -1,19 +1,30 @@
 import express from 'express'
 import cors from "cors"
+import mysql from 'mysql'
 
 const app = express()
 const port = 3000
-/*
+
+const connection = mysql.createConnection({
+})
+
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.setHeader('Access-Control-Allow-Methods', '*');
     res.setHeader("Access-Control-Allow-Headers", "*");
     next();
 });
-*/
 
-app.use(cores)
 
+connection.connect();
+
+connection.query('SELECT * FROM rooms', (err, resalt) => {
+    if (err) throw err
+
+    console.log(resalt)
+})
+
+connection.end()
 
 app.get('/api/rooms', (req, res) => {
     res.send([
@@ -26,7 +37,6 @@ app.get('/api/rooms', (req, res) => {
         }
     ])
 })
-
 
 
 app.listen(port, () => {
